1a9e7f4b629199674bad96053c8c3e2fc861aa34
Hari <smsharisankar@gmail.com>
2020-05-12 19:19:22 +0530

    Added Vertical Pill Tab dynamic component template

---
+++ src/components/common/VerticalPillTabs/VerticalPillTabsComponent.tsx
@@ -0,0 +1,100 @@
+import React, { Component } from "react";
+import {Nav, Tab, Row, Col} from 'react-bootstrap';
+import _ from "lodash";
+import "./VerticalPillTabs.scss";
+export interface Props {
+	title?: string;
+	children:React.ReactElement[];
+}
+export interface tabDetailProps{
+	title:string,
+	content:string,
+	key:string
+}
+export default class VerticalPillTabsComponent extends Component {
+	selected = '';
+	getTabConfigs():tabDetailProps[]{
+		let data:any[] = [];
+		// let children = React.Children.toArray(this.props.children);
+		if(this.props.children){
+			let title = '';
+			React.Children.forEach(this.props.children,(child,i)=>{
+				if (!React.isValidElement(child)) {
+					return child
+				  }
+				// To get Component Name of Child Component
+				_.forOwn(child,function(val,key){
+					if(key === 'type'){
+						title = _.startCase(val['name']);
+					}
+				});
+				// Format the Component Props based propertyies
+				if(child.props.active){
+					this.selected = title+i;
+				}
+				data.push(
+					{
+						title: title,
+						key: title+i,
+						content:child
+					}
+				);
+			});
+		}
+		return data;
+		// return (
+		// 	[{
+		// 		title:'Tab Name 1',
+		// 		content:'This is Tab Content 1',
+		// 		key:'DropdownMultiMenu'
+		// 	},
+		// 	{
+		// 		title:'Tab Name 2',
+		// 		content:'This is Tab Content 2',
+		// 		key:'DropdownMenu'
+		// 	},
+		// 	{
+		// 		title:'Tab Name 3',
+		// 		content:'This is Tab Content 3',
+		// 		key:'Dropdown'
+		// 	}]
+		// );
+
+	}
+	render() {
+		let config = this.getTabConfigs();
+		let navItem:any = [];
+		let tabPane:any = [];
+		config.forEach((value:tabDetailProps,index:number)=>{
+			navItem.push(
+				<Nav.Item key={index}>
+					<Nav.Link eventKey={value.key}>{_.startCase(value.title)}</Nav.Link>
+				</Nav.Item>
+			);
+			tabPane.push(
+				<Tab.Pane key={index} eventKey={value.key}>
+					{value.content}
+				</Tab.Pane>
+			);
+		});
+		console.log(config);
+		return (
+			<div className="custom-container">
+				<Tab.Container id="left-tabs-example" defaultActiveKey={this.selected}>
+					<Row>
+						<Col sm={3}>
+						<Nav variant="pills" className="flex-column">
+							{navItem}
+						</Nav>
+						</Col>
+						<Col sm={9}>
+						<Tab.Content className="custom-container p-4 tab-content-container">
+							{tabPane}
+						</Tab.Content>
+						</Col>
+					</Row>
+				</Tab.Container>
+			</div>
+		);
+	}
+}
--- src/pages/components/components.page.tsx
+++ src/pages/components/components.page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { RouteComponentProps } from '@reach/router';
 import './components.scss';
 import DropdownMultiMenu from  '../../components/common/DropdownMultiMenu';
+import VerticalPillComponent from '../../components/common/VerticalPillTabs'
 import loadData from '../../data/dropdown-multi-menu.data.json';
 
 const dropDownData:any = loadData;
@@ -10,7 +11,10 @@ const Components = (props:RouteComponentProps)=> {
         <div>
             <h2 className="text-center">Components</h2>
             <div>
-                <DropdownMultiMenu title="Select Department" data={dropDownData}/>
+                <VerticalPillComponent>
+                    <DropdownMultiMenu active={true} domKey="1" title="Select Department" data={dropDownData}/>
+                    {/* <DropdownMultiMenu active={true} domKey="2" title="Select User" data={dropDownData}/> */}
+                </VerticalPillComponent>
             </div>
         </div>
     );

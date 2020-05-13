6e1299cdd4917612f343db1e52430012cf7e0494
Hari <smsharisankar@gmail.com>
2020-05-13 09:29:15 +0530

    Added accordion view for VerticalPillTab Component - Props Object pretty view css

--- src/components/common/VerticalPillTabs/VerticalPillTabsComponent.tsx
+++ src/components/common/VerticalPillTabs/VerticalPillTabsComponent.tsx
@@ -1,47 +1,47 @@
 import React, { Component } from "react";
-import {Nav, Tab, Row, Col} from 'react-bootstrap';
+import { Nav, Tab, Row, Col, Card, Accordion } from "react-bootstrap";
 import _ from "lodash";
 import "./VerticalPillTabs.scss";
 export interface Props {
 	title?: string;
-	children:React.ReactElement[];
+	children: React.ReactElement[];
 }
-export interface tabDetailProps{
-	title:string,
-	content:string,
-	key:string
+export interface tabDetailProps {
+	title: string;
+	content: string;
+	key: string;
 }
 export default class VerticalPillTabsComponent extends Component {
-	selected = '';
-	getTabConfigs():tabDetailProps[]{
-		let data:any[] = [];
+	selected = "";
+	childProps:any[] = [];
+	getTabConfigs(): tabDetailProps[] {
+		let data: any[] = [];
 		// let children = React.Children.toArray(this.props.children);
-		if(this.props.children){
-			let title = '';
-			React.Children.forEach(this.props.children,(child,i)=>{
+		if (this.props.children) {
+			let title = "";
+			React.Children.forEach(this.props.children, (child, i) => {
 				if (!React.isValidElement(child)) {
-					return child
-				  }
+					return child;
+				}
+				this.childProps.push(JSON.stringify(child.props, null, 2));
 				// To get Component Name of Child Component
-				title = _.get(child, 'type.componentName');
-				title = (title)?title:'CustomComponent';
-				console.log('Component Name is:',title);
+				title = _.get(child, "type.componentName");
+				title = title ? title : "CustomComponent";
+				console.log("Component Name is:", title);
 				// _.forOwn(child,function(val,key){
 				// 	if(key === 'type'){
 				// 		title = _.startCase(val['name']);
 				// 	}
 				// });
 				// Format the Component Props based propertyies
-				if(child.props.active){
-					this.selected = title+i;
+				if (child.props.active) {
+					this.selected = title + i;
 				}
-				data.push(
-					{
-						title: title,
-						key: title+i,
-						content:child
-					}
-				);
+				data.push({
+					title: title,
+					key: title + i,
+					content: child,
+				});
 			});
 		}
 		return data;
@@ -62,16 +62,17 @@ export default class VerticalPillTabsComponent extends Component {
 		// 		key:'Dropdown'
 		// 	}]
 		// );
-
 	}
 	render() {
 		let config = this.getTabConfigs();
-		let navItem:any = [];
-		let tabPane:any = [];
-		config.forEach((value:tabDetailProps,index:number)=>{
+		let navItem: any = [];
+		let tabPane: any = [];
+		config.forEach((value: tabDetailProps, index: number) => {
 			navItem.push(
 				<Nav.Item key={index}>
-					<Nav.Link eventKey={value.key}>{_.startCase(value.title)}</Nav.Link>
+					<Nav.Link eventKey={value.key}>
+						{_.startCase(value.title)}
+					</Nav.Link>
 				</Nav.Item>
 			);
 			tabPane.push(
@@ -83,17 +84,52 @@ export default class VerticalPillTabsComponent extends Component {
 		console.log(config);
 		return (
 			<div className="custom-container">
-				<Tab.Container id="left-tabs-example" defaultActiveKey={this.selected}>
+				<Tab.Container
+					id="left-tabs-example"
+					defaultActiveKey={this.selected}
+				>
 					<Row>
 						<Col sm={3}>
-						<Nav variant="pills" className="flex-column">
-							{navItem}
-						</Nav>
+							<Nav variant="pills" className="flex-column">
+								{navItem}
+							</Nav>
 						</Col>
 						<Col sm={9}>
-						<Tab.Content className="custom-container p-4 tab-content-container">
-							{tabPane}
-						</Tab.Content>
+							{/* Accordion Section */}
+							<Accordion defaultActiveKey="0">
+								<Card>
+									<Accordion.Toggle
+										as={Card.Header}
+										eventKey="0"
+									>
+										Component
+									</Accordion.Toggle>
+									<Accordion.Collapse eventKey="0">
+										<Card.Body>
+										<Tab.Content className="height-250">
+											{tabPane}
+										</Tab.Content>
+										</Card.Body>
+									</Accordion.Collapse>
+								</Card>
+								<Card>
+									<Accordion.Toggle
+										as={Card.Header}
+										eventKey="1"
+									>
+										Props
+									</Accordion.Toggle>
+									<Accordion.Collapse eventKey="1">
+										<Card.Body>
+											<div>
+												<pre className="pretty-custom">
+													{this.childProps}
+												</pre>
+											</div>
+										</Card.Body>
+									</Accordion.Collapse>
+								</Card>
+							</Accordion>
 						</Col>
 					</Row>
 				</Tab.Container>
